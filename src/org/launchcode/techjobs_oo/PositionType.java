952a84b773bc7278e1a398efae06da8d2701fdba
denysV1978 <denysvuk@gmail.com>
2019-11-30 10:49:27 -0600

    Parts 1 and 2 are done.

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs_oo;
 
+import java.util.Objects;
+
 public class PositionType {
     private int id;
     private static int nextId = 1;
@@ -17,8 +19,28 @@ public class PositionType {
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == null) return false; // null check
+        if(o.getClass() != this.getClass()) return false; //class check
+        if(!(o instanceof PositionType)) return false; // another way to do class check
+        if(o == this) return true; //reference check - on the base of address
+        PositionType positionType = (PositionType) o; //casting object o to PositionType Class
+        return positionType.getId() == this.getId();  //compare two objects on the base of their IDs.
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 
     // Getters and Setters:
 
